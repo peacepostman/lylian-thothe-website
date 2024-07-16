84357b983bfc7d529ae32c0158479d09e42c903a
lytianoleth <168477171+lytianoleth@users.noreply.github.com>
2024-07-16 10:48:11 +0200

    IDK

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -10,7 +10,7 @@ export const onRouteUpdate = () => {
   if (window.Masonry) {
     var elem = document.querySelector('.grid');
     if (elem) {
-      var msnry = new Masonry('.grid', { itemSelector: '[data-item]', columnWidth: '[data-grid-sizer]', gutter: 10 });
+      var msnry = new Masonry('.grid', { itemSelector: '[data-item]', columnWidth: '[data-grid-sizer]', gutter: 0 });
       msnry.layout();
     }
   }
